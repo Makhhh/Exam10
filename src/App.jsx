827e9e55bf52e1c95011c3a3a4fcd32446cb2d1f
Makhhh <makhmarkhov@gmail.com>
2025-04-21 17:21:22 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Emty10 from './components/Emty10'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Emty10/>
+      </div>
+    </>
+  )
+}
+
+export default App
